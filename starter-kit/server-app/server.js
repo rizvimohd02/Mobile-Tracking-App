e0bc736472b6b8402c018f20c9008d1f45b98818
rizvimohd02 <68018392+rizvimohd02@users.noreply.github.com>
2021-02-08 19:45:44 +0530

    Update server.js

--- starter-kit/server-app/server.js
+++ starter-kit/server-app/server.js
@@ -59,55 +59,6 @@ app.get('/api/session', (req, res) => {
 });
 
 /**
- * Post process the response from Watson Assistant
- *
- * We want to see if this was a request for resources/supplies, and if so
- * look up in the Cloudant DB whether any of the requested resources are
- * available. If so, we insert a list of the resouces found into the response
- * that will sent back to the client.
- * 
- * We also modify the text response to match the above.
- */
-function post_process_assistant(result) {
-  let resource
-  // First we look to see if a) Watson did identify an intent (as opposed to not
-  // understanding it at all), and if it did, then b) see if it matched a supplies entity
-  // with reasonable confidence. "supplies" is the term our trained Watson skill uses
-  // to identify the target of a question about resources, i.e.:
-  //
-  // "Where can i find face-masks?"
-  //
-  // ....should return with an enitity == "supplies" and entitty.value = "face-masks"
-  //
-  // That's our trigger to do a lookup - using the entitty.value as the name of resource
-  // to to a datbase lookup.
-  if (result.intents.length > 0 ) {
-    result.entities.forEach(item => {
-      if ((item.entity == "supplies") &&  (item.confidence > 0.3)) {
-        resource = item.value
-      }
-    })
-  }
-  if (!resource) {
-    return Promise.resolve(result)
-  } else {
-    // OK, we have a resource...let's look this up in the DB and see if anyone has any.
-    return cloudant
-      .find('', resource, '')
-      .then(data => {
-        let processed_result = result
-        if ((data.statusCode == 200) && (data.data != "[]")) {
-          processed_result["resources"] = JSON.parse(data.data)
-          processed_result["generic"][0]["text"] = 'There is' + '\xa0' + resource + " available"
-        } else {
-          processed_result["generic"][0]["text"] = "Sorry, no" + '\xa0' + resource + " available"           
-        }
-        return processed_result
-      })
-  }
-}
-
-/**
  * Post a messge to Watson Assistant
  *
  * The body must contain:
@@ -131,73 +82,31 @@ app.post('/api/message', (req, res) => {
 });
 
 /**
- * Get a list of resources
- *
- * The query string may contain the following qualifiers:
- * 
- * - type
- * - name
- * - userID
- *
- * A list of resource objects will be returned (which can be an empty list)
- */
-app.get('/api/resource', (req, res) => {
-  const type = req.query.type;
-  const name = req.query.name;
-  const userID = req.query.userID;
-  cloudant
-    .find(type, name, userID)
-    .then(data => {
-      if (data.statusCode != 200) {
-        res.sendStatus(data.statusCode)
-      } else {
-        res.send(data.data)
-      }
-    })
-    .catch(err => handleError(res, err));
-});
-
-/**
- * Create a new resource
+ * Create a new entry
  *
  * The body must contain:
  * 
- * - type
  * - name
  * - contact
  * - userID
- *
- * The body may also contain:
- * 
- * - description
- * - quantity (which will default to 1 if not included)
- * 
  * The ID and rev of the resource will be returned if successful
  */
-//let types = ["Food", "Other", "Help"]
 app.post('/api/resource', (req, res) => {
-  // if (!req.body.type) {
-  //   return res.status(422).json({ errors: "Type of item must be provided"});
-  // }
-  // if (!types.includes(req.body.type)) {
-  //   return res.status(422).json({ errors: "Type of item must be one of " + types.toString()});
-  // }
+
   if (!req.body.name) {
-    return res.status(422).json({ errors: "Name of item must be provided"});
+    return res.status(422).json({ errors: "Name of associate must be provided"});
   }
   if (!req.body.contact) {
     return res.status(422).json({ errors: "A method of contact must be provided"});
   }
-  //const type = req.body.type;
   const name = req.body.name;
   const description = req.body.description || '';
   const userID = req.body.userID || '';
-  //const quantity = req.body.quantity || 1;
   const location = req.body.location || '';
   const contact = req.body.contact;
-
+  const trnsctype = req.body.trnsctype;
   cloudant
-    .create(name, description, location, contact, userID)
+    .create(name, description, location, contact, userID, trnsctype)
     .then(data => {
       if (data.statusCode != 201) {
         res.sendStatus(data.statusCode)
@@ -205,46 +114,6 @@ app.post('/api/resource', (req, res) => {
         res.send(data.data)
       }
     })
-    .catch(err => handleError(res, err));
-});
-
-/**
- * Update new resource
- *
- * The body may contain any of the valid attributes, with their new values. Attributes
- * not included will be left unmodified.
- * 
- * The new rev of the resource will be returned if successful
- */
-
-app.patch('/api/resource/:id', (req, res) => {
-  const type = req.body.type || '';
-  const name = req.body.name || '';
-  const description = req.body.description || '';
-  const userID = req.body.userID || '';
-  const quantity = req.body.quantity || '';
-  const location = req.body.location || '';
-  const contact = req.body.contact || '';
-
-  cloudant
-    .update(req.params.id, type, name, description, quantity, location, contact, userID)
-    .then(data => {
-      if (data.statusCode != 200) {
-        res.sendStatus(data.statusCode)
-      } else {
-        res.send(data.data)
-      }
-    })
-    .catch(err => handleError(res, err));
-});
-
-/**
- * Delete a resource
- */
-app.delete('/api/resource/:id', (req, res) => {
-  cloudant
-    .deleteById(req.params.id)
-    .then(statusCode => res.sendStatus(statusCode))
     .catch(err => handleError(res, err));
 });
 
