6ab48ee3ddf674fbb7f973166c6dd260af2d4318
rizvimohd02 <68018392+rizvimohd02@users.noreply.github.com>
2021-02-08 19:44:56 +0530

    Update cloudant.js

--- starter-kit/server-app/lib/cloudant.js
+++ starter-kit/server-app/lib/cloudant.js
@@ -71,100 +71,30 @@ const dbCloudantConnect = () => {
 })();
 
 /**
- * Find all resources that match the specified partial name.
- * 
- * @param {String} type
- * @param {String} partialName
- * @param {String} userID
- * 
- * @return {Promise} Promise - 
- *  resolve(): all resource objects that contain the partial
- *          name, type or userID provided, or an empty array if nothing
- *          could be located that matches. 
- *  reject(): the err object from the underlying data store
- */
-function find(type, partialName, userID) {
-    return new Promise((resolve, reject) => {
-        let selector = {}
-        if (type) {
-            selector['type'] = type;
-        }
-        if (partialName) {
-            let search = `(?i).*${partialName}.*`;
-            selector['name'] = {'$regex': search};
-
-        }
-        if (userID) {
-            selector['userID'] = userID;
-        }
-        
-        db.find({ 
-            'selector': selector
-        }, (err, documents) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ data: JSON.stringify(documents.docs), statusCode: 200});
-            }
-        });
-    });
-}
-
-/**
- * Delete a resource that matches a ID.
- * 
- * @param {String} id
- * 
- * @return {Promise} Promise - 
- *  resolve(): Status code as to whether to the object was deleted
- *  reject(): the err object from the underlying data store
- */
-function deleteById(id, rev) {
-    return new Promise((resolve, reject) => {
-        db.get(id, (err, document) => {
-            if (err) {
-                resolve(err.statusCode);
-            } else {
-                db.destroy(id, document._rev, (err) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(200);
-                    }
-                })
-            }            
-        })
-    });
-}
-
-/**
  * Create a resource with the specified attributes
  * 
- * @param {String} type - the type of the item
- * @param {String} name - the name of the item
- * @param {String} description - the description of the item
- * @param {String} quantity - the quantity available 
+ * @param {String} name - the name of the associate
+ * @param {String} description - Any comment to be added
  * @param {String} location - the GPS location of the item
  * @param {String} contact - the contact info 
  * @param {String} userID - the ID of the user 
- * 
+ * @param {String} trnsctype - the type of transaction
  * @return {Promise} - promise that will be resolved (or rejected)
  * when the call to the DB completes
  */
-function create(name, description, location, contact, userID) {
+function create(name, description, location, contact, userID, trnsctype) {
     return new Promise((resolve, reject) => {
         let itemId = uuidv4();
         let whenCreated = Date.now();
         let item = {
             _id: itemId,
             id: itemId,
-            //type: type,
             name: name,
             description: description,
-            //quantity: quantity,
             location: location,
             contact: contact,
             userID: userID,
+            trnsctype: trnsctype,
             whenCreated: whenCreated
         };
         db.insert(item, (err, result) => {
@@ -178,54 +108,6 @@ function create(name, description, location, contact, userID) {
     });
 }
 
-/**
- * Update a resource with the requested new attribute values
- * 
- * @param {String} id - the ID of the item (required)
- * 
- * The following parameters can be null
- * 
- * @param {String} type - the type of the item
- * @param {String} name - the name of the item
- * @param {String} description - the description of the item
- * @param {String} quantity - the quantity available 
- * @param {String} location - the GPS location of the item
- * @param {String} contact - the contact info 
- * @param {String} userID - the ID of the user 
- * 
- * @return {Promise} - promise that will be resolved (or rejected)
- * when the call to the DB completes
- */
-function update(id, type, name, description, quantity, location, contact, userID) {
-    return new Promise((resolve, reject) => {
-        db.get(id, (err, document) => {
-            if (err) {
-                resolve({statusCode: err.statusCode});
-            } else {
-                let item = {
-                    _id: document._id,
-                    _rev: document._rev,            // Specifiying the _rev turns this into an update
-                }
-                if (type) {item["type"] = type} else {item["type"] = document.type};
-                if (name) {item["name"] = name} else {item["name"] = document.name};
-                if (description) {item["description"] = description} else {item["description"] = document.description};
-                if (quantity) {item["quantity"] = quantity} else {item["quantity"] = document.quantity};
-                if (location) {item["location"] = location} else {item["location"] = document.location};
-                if (contact) {item["contact"] = contact} else {item["contact"] = document.contact};
-                if (userID) {item["userID"] = userID} else {item["userID"] = document.userID};
- 
-                db.insert(item, (err, result) => {
-                    if (err) {
-                        console.log('Error occurred: ' + err.message, 'create()');
-                        reject(err);
-                    } else {
-                        resolve({ data: { updatedRevId: result.rev }, statusCode: 200 });
-                    }
-                });
-            }            
-        })
-    });
-}
 
 function info() {
     return cloudant.db.get(db_name)
@@ -236,9 +118,6 @@ function info() {
 };
 
 module.exports = {
-    deleteById: deleteById,
     create: create,
-    update: update,
-    find: find,
     info: info
   };
